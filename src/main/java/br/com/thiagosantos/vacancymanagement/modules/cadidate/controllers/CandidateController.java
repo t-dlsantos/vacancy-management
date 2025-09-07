379294be5baaa08b489576383337dcd9a2a4da4f
Thiago Santos <thiagodlsantos9@gmail.com>
2025-09-07 20:33:30 -0300

    feat: initial Candidate setup

---
+++ src/main/java/br/com/thiagosantos/vacancymanagement/modules/cadidate/controllers/CandidateController.java
@@ -0,0 +1,17 @@
+package br.com.thiagosantos.vacancymanagement.modules.cadidate.controllers;
+
+import br.com.thiagosantos.vacancymanagement.modules.cadidate.CandidateEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+
+    @PostMapping("/")
+    public void create(@RequestBody CandidateEntity candidate) {
+        System.out.println("Candidate email: " + candidate.getEmail());
+    }
+}
---
+++ src/test/java/br/com/thiagosantos/vacancymanagement/VacancyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiagosantos.vacancymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
