8508fb12d595bcadeb684f2a61471cff66a908f8
Thiago Santos <thiagodlsantos9@gmail.com>
2025-09-11 14:30:13 -0300

    feat: add Candidate password encryption

--- src/main/java/br/com/thiagosantos/vacancymanagement/modules/cadidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/thiagosantos/vacancymanagement/modules/cadidate/useCases/CreateCandidateUseCase.java
@@ -4,18 +4,29 @@ import br.com.thiagosantos.vacancymanagement.exceptions.UserFoundException;
 import br.com.thiagosantos.vacancymanagement.modules.cadidate.entities.CandidateEntity;
 import br.com.thiagosantos.vacancymanagement.modules.cadidate.repositories.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreateCandidateUseCase {
 
-    @Autowired
-    private CandidateRepository candidateRepository;
+    private final CandidateRepository candidateRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public CreateCandidateUseCase(CandidateRepository candidateRepository, PasswordEncoder passwordEncoder) {
+        this.candidateRepository = candidateRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public CandidateEntity execute(CandidateEntity candidateEntity) {
-        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail()).ifPresent(user -> {
-            throw new UserFoundException();
-        });
+        this.candidateRepository
+                .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent(user -> {
+                    throw new UserFoundException();
+                });
+
+        var password = passwordEncoder.encode(candidateEntity.getPassword());
+        candidateEntity.setPassword(password);
 
         return this.candidateRepository.save(candidateEntity);
     }
