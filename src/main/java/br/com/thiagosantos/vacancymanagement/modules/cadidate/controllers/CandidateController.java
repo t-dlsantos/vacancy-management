13212046f59b515b6fbad76232960cbda29b21a6
Thiago Santos <thiagodlsantos9@gmail.com>
2025-09-14 17:04:01 -0300

    chore: remove unused import statements

--- src/main/java/br/com/thiagosantos/vacancymanagement/modules/cadidate/controllers/CandidateController.java
+++ src/main/java/br/com/thiagosantos/vacancymanagement/modules/cadidate/controllers/CandidateController.java
@@ -1,6 +1,5 @@
 package br.com.thiagosantos.vacancymanagement.modules.cadidate.controllers;
 
-import br.com.thiagosantos.vacancymanagement.modules.cadidate.dto.ProfileCandidateResponseDTO;
 import br.com.thiagosantos.vacancymanagement.modules.cadidate.entities.CandidateEntity;
 import br.com.thiagosantos.vacancymanagement.modules.cadidate.useCases.CreateCandidateUseCase;
 import br.com.thiagosantos.vacancymanagement.modules.cadidate.useCases.ListAllJobsByFilterUseCase;
@@ -16,8 +15,6 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
